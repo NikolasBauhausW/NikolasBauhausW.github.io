1d8adec3aee25c2007eb38234689890d840a8ea4
unknown <ncbauhaus@willamette.edu>
2024-04-24 13:41:54 -0700

    fizzbuzz

---
+++ stoopidassignment.py
@@ -0,0 +1,18 @@
+import math
+
+class encrytor():
+
+    def __init__(self, pubKey, privKey) -> None): 
+        
+        self.pubKey = pubKey
+        self.privKey = privKey
+    
+    def textToNum():
+
+    def numToText():
+
+    def encrypt():
+
+    def decrpyt():
+
+        
